b60267e9e8b0059aad0df11936b87321640a36db
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-07-28 15:58:26 +0100

    Wrote utility functions to process the uploaded results

--- WebApp/utils/utils.py
+++ WebApp/utils/utils.py
@@ -1,92 +1,24 @@
 import re
-import bcrypt
-import random
-import itsdangerous
-import configparser
-
+import io
 import pandas as pd
 
-from smtplib import SMTP
-from email.message import EmailMessage
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from typing import Dict, Tuple
 
 
-config = configparser.ConfigParser()
-config.read('configfile.ini')
-
-EMAIL = config['email']['addr']
-PASSWORD = config['email']['pwd']
-SECRET_KEY = config['key']['secret_key']
 
 
-def send_email_code(message: EmailMessage):
-    """This function connects to the SMTP server, and sends the email `message`
-    to the user - `recv`."""
-    server = SMTP(host='smtp.googlemail.com', port=587)
-    server.starttls()
-    server.login(EMAIL, PASSWORD)
-    server.send_message(message)
+def get_semester(df):
+    """Gets and return the semester of the result as an
+    integer from the dataframe."""
+    course = df.index[0]
+    course_title = df.loc[course]['Title']
+    if "SWEP" in course_title:
+        return 3
+    last_dgt = int(course[-1])
+    return 2 if last_dgt % 2 == 0 else 1
 
 
-# noinspection SpellCheckingInspection
-def create_recovery_email(code: str, recv: str, sender: str = 'noreply@gmail.com', reset=True) -> EmailMessage:
-    """This function composes the reset password / email verification message to be sent to the 'recv'.
-    : return: email object"""
-    content = f'''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
-    </head>
-    <body bgcolor="#fff" style="font-family: 'Calibri';font-weight: 200px;font-size: 25px;margin-top:0;
-    margin-bottom:0 ;margin-right:0 ;margin-left:0 ;padding-top:0px;
-    padding-bottom:0px;padding-right:0px;padding-left:0px;background-color:#e1e5e8;">
-        <center style="width:100%;table-layout:fixed;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;
-        background-color:#fff;">
-        <div style="max-width:600px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;">
-            <p>Your {"password reset" if reset else "email verification"} pin is:<br>
-            <span style="font-size: 65px; color: #2222ff;">{code}</span><br><br>
-            <small><span style="color: #ff0000">NOTE:</span> This code expires after <b>10 minutes</b>.<br>
-            Please, {"ignore this message if you didn't request for a change of password." if reset else
-            "enter the pin to verify your email."}</small></p>
-        </div>
-        </center>
-    </body>
-    </html>
-    '''
-    email = EmailMessage()
-
-    email['Subject'] = f'{"Password Reset" if reset else "Account Verification"} Pin'
-    email['From'] = sender
-    email['To'] = recv
-
-    email.set_content(content, subtype='html')
-    return email
-
-
-def generate_token(email: str) -> Dict[str, bytes]:
-    """This function generates a token which expires after 10 minutes. This token is
-    to reset the password."""
-    s = Serializer(SECRET_KEY, expires_in=600)  # creates a Serializer which expires in 10 minutes
-    token = s.dumps(email)
-    code = str(random.randint(100000, 999999))
-    return {code: token}
-
-
-def validate_token(token: bytes):
-    """This function validates the `token` and checks if it is still valid (i.e not expired yet)."""
-    s = Serializer(SECRET_KEY)
-    try:
-        email = s.loads(token)
-        return email
-    except itsdangerous.exc.SignatureExpired:
-        return None
-
-
-def parse_html(file, sessional=True) -> Tuple:
+def parse_result_html(file) -> Tuple:
     """
     This function parses the HTML file containing the results of a Student (esp. University of Ilorin)
     into a pandas DataFrame and creates a Semester object for each individual semester contained in the result.
@@ -102,12 +34,15 @@ def parse_html(file, sessional=True) -> Tuple:
         'E': 1,
         'F': 0
         }
-    matric, name, fac, dept, level = pd.read_html(file)[0][1]
+    # the original names of the variables
+    # matric, name, fac, dept, level = pd.read_html(file)[0][1]
+    _, _, fac, _, level = pd.read_html(file)[0][1]
     
     # extracting the session id
-    with open(file, 'r') as f:
-        content = f.read()
-        session_id = re.search('\d\d\d\d/\d\d\d\d', content).group()
+    # decoding the `file` object which is an io.BytesIO object
+    # content = file.getvalue().decode()
+    # session = re.search('\d\d\d\d/\d\d\d\d', content).group()
+    session = '2017/2018'
 
     dfs = pd.read_html(file, header=1, index_col=1)
     for df in dfs[1:]:
@@ -121,25 +56,19 @@ def parse_html(file, sessional=True) -> Tuple:
             index=df.index,
             dtype='int8'
             )
-    if sessional:
-        return dfs[1:], session_id, name, matric, fac, dept, level
-    return dfs[-1], session_id, name, matric, fac, dept, level
 
-def parse(file, sessional=True) -> Tuple:
-    """
-    This function parses the HTML file containing the results of a Student (esp. University of Ilorin)
-    into a pandas DataFrame and creates a Semester object for each individual semester contained in the result.
-    Ensure the HTML files are stored in a folder named 'results' and the filenames are named in ascending order 
-    according to their corresponding years to enable the function locate it.
-    :return: List[pandas.DataFrame]
-    """
-    grade = {
-        'A': 5,
-        'B': 4,
-        'C': 3,
-        'D': 2,
-        'E': 1,
-        'F': 0
-        }
-    matric, name, fac, dept, level = pd.read_html(file)[0][1]
-    dfs = pd.read_html(file, header=1, index_col=1)
+    return dfs[1:], session, level
+
+
+def calculate_gpa(df: pd.DataFrame) -> float:
+    """Calculates the GPA of a semester result.
+    params:
+        df: pandas.DataFrame object
+    returns:
+        Returns the GPA as a floating point number."""
+    gradients = df['Gradient']
+    units = df['Unit']
+    try:
+        return sum(gradients) / sum(units)
+    except ZeroDivisionError:
+        return float(0)
