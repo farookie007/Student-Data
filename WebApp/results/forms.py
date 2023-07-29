7870374f36d463c2f8ff199208b1bbe397e24dcd
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-07-29 06:44:36 +0100

    Tweaked the form to accept multiple files

--- WebApp/results/forms.py
+++ WebApp/results/forms.py
@@ -1,5 +1,26 @@
 from django import forms
 
 
-class FileUploadForm(forms.Form):
-    file = forms.FileField(allow_empty_file=False, required=True)
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+    
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
+class ResultUploadForm(forms.Form):
+    file = MultipleFileField(
+        allow_empty_file=False,
+        required=True,
+    )
