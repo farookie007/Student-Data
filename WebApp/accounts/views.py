e6934823c4cff4e823498a8cbd2f6d63df9d1ae1
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-07-30 15:41:43 +0100

    Created the `register` view to sign up users

--- WebApp/accounts/views.py
+++ WebApp/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You\'ll be redirected to login')
+            return redirect(reverse('user_auth:login'))
+
+    form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
