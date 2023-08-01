a3a63aad921a4f8cec50f72451cf55769e929a7e
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-01 10:00:24 +0100

    Created the `LoginForm`

---
+++ WebApp/user_auth/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+
+
+class LoginForm(forms.Form):
+    matric = forms.CharField(max_length=15, required=True, strip=True)
+    password = forms.CharField(widget=forms.PasswordInput())
