823638840397d0fb44dc83ae61ca2db1658de3fe
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-01 02:45:40 +0100

    Added alert messages to the views and created the `dashboard` view

--- WebApp/accounts/views.py
+++ WebApp/accounts/views.py
@@ -13,6 +13,12 @@ def register(request):
             form.save()
             messages.success(request, 'Account created successfully. You\'ll be redirected to login')
             return redirect(reverse('user_auth:login'))
-
+        form = CustomUserCreationForm(request.POST)
+        messages.warning(request, "Invalid parameters")
+        return render(request, 'accounts/register.html', {'form': form})
     form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
