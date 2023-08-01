ef0326b8c0088ef76a715f90825c8859dd277b29
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-01 02:47:28 +0100

    Updated the project level settings
    Added the `widget_tweaks` app to INSTALLED_APPS
    Added the root directory for static files
    Added config values for login

--- WebApp/result_manager/settings.py
+++ WebApp/result_manager/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'user_auth.apps.UserAuthConfig',
     # 3rd party Apps
     'django_extensions',    # provides a debugging console on the error page
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -132,3 +136,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'accounts:dashboard'
+LOGIN_URL = 'user_auth:login'
