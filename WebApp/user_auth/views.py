f62d907c5abb832fba0358ac064cf73f3650f76e
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-01 09:03:06 +0100

    Created the login view

--- WebApp/user_auth/views.py
+++ WebApp/user_auth/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+from .forms import LoginForm
+
+
+
+def login(request):
+    if request.method == 'POST':
+        # form = LoginForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            matric = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            # getting the auto-generated username using the matric_no
+            # user = get_user_model().objects.filter(matric=matric).first()
+            user = authenticate(request, username=matric, password=password)
+            print(user)
+            if user is not None:
+                login(request)
+                messages.success(request, f'Login successful')
+                return redirect(reverse('accounts:dashboard'))
+        messages.error(request, 'Invalid matric number or password')
+        # form = LoginForm(request.POST)
+        form = AuthenticationForm(request.POST)
+    else:
+        # form = LoginForm()
+        form = AuthenticationForm()
+    return render(request, 'user_auth/login.html', {'form': form})
